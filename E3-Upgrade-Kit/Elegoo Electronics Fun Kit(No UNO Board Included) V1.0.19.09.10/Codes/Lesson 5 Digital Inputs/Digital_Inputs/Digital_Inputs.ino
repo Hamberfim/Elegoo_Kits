0f7ecab3d530d5ecdf0e0dc7e6343d8366d21bf8
Hamberfim <adhamlingithub@gmail.com>
2023-12-14 11:47:01 -0600

    e3 kit

---
+++ E3-Upgrade-Kit/Elegoo Electronics Fun Kit(No UNO Board Included) V1.0.19.09.10/Codes/Lesson 5 Digital Inputs/Digital_Inputs/Digital_Inputs.ino
@@ -0,0 +1,28 @@
+//www.elegoo.com
+//2016.12.08
+
+int ledPin = 5;
+int buttonApin = 3;
+int buttonBpin = 4;
+
+byte leds = 0;
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonApin, INPUT_PULLUP);  
+  pinMode(buttonBpin, INPUT_PULLUP);  
+}
+
+void loop() 
+{
+  if (digitalRead(buttonApin) == LOW)
+  {
+    digitalWrite(ledPin, HIGH);
+    delay(5000);
+  }
+  if (digitalRead(buttonBpin) == LOW)
+  {
+    digitalWrite(ledPin, LOW);
+  }
+}
